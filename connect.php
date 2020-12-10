dfb1a654557dc7f00d1f1bc55a39e402d5023557
NoemiZarate <73940535+NoemiZarate@users.noreply.github.com>
2020-12-11 07:31:21 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = " dbProducts";
+	$dbname = " dbFacebook";
 
 
 	$connect = mysqli_connect($servername,$username,$password,$dbname);
